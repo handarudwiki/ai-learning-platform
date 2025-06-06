c8a47b1797b3e52ff72db2352b345811f64ffc9f
handarudwiki <handarudwiki04@gmail.com>
2025-06-06 19:00:00 +0700

    feat: add folter subject adn search in list components

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,6 +1,26 @@
+"use client"
+
 import Image from "next/image"
+import { useRouter, useSearchParams } from "next/navigation"
+import { useEffect, useState } from "react"
 
 const SearchInput = () => {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const query = searchParams.get("topic") || ""
+  const [search, setSearch] = useState(query)
+
+  useEffect(() => {
+    const params = new URLSearchParams(searchParams.toString())
+    if (search.trim() === "") {
+      params.delete("topic")
+    }else {
+      params.set("topic", search.trim())
+    }
+    const newUrl = `?${params.toString()}`
+    router.push(newUrl)
+    },[search])
+
   return (
     <div className="relative border border-black rounded-lg items-center flex gap-2 px-2 py-1 h-fit">
         <Image
@@ -9,7 +29,7 @@ const SearchInput = () => {
         width={15}
         height={15}
         />
-        <input type="text" className="outline-none" placeholder="Searchc ompanions..." />
+        <input type="text" className="outline-none" placeholder="Searchc ompanions..." value={search} onChange={(e)=> setSearch(e.target.value)} />
     </div>
   )
 }
--- components/SubjectFilter.tsx
+++ components/SubjectFilter.tsx
@@ -1,9 +1,36 @@
+"use client"
+
 import { subjects } from "@/constants"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select"
 
+import { useRouter, useSearchParams } from "next/navigation"
+import { useEffect, useState } from "react"
+
 const SubjectFilter = () => {
+    const router = useRouter()
+    const searchParams = useSearchParams()
+    const query = searchParams.get("subject") || ""
+
+    const [subject, setSubject] = useState(query)
+
+    console.log("SubjectFilter", subject)
+
+    useEffect(() => {
+        const params = new URLSearchParams(searchParams.toString())
+
+        let newUrl = ""
+        if (subject == "all"){
+            params.delete("subject")
+        }else {
+            params.set("subject", subject)
+        } 
+        newUrl = `?${params.toString()}`
+        router.push(newUrl)
+    },[subject])
+
+       
   return (
-    <Select>
+    <Select onValueChange={setSubject} value={subject}>
         <SelectTrigger className="input capitalize">
             <SelectValue placeholder="Subject" />
         </SelectTrigger>
